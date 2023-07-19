223108d263f8a65f194f35604dcd0df668e01111
krantamaki <kasper.rantamaki@csc.fi>
2023-07-19 16:05:25 +0300

    Fixes

--- solver/src/linsolve.cpp
+++ solver/src/linsolve.cpp
@@ -3,6 +3,7 @@
 #include "../../lalib/src/nonstationarySolvers.hpp"
 #include "../../lalib/src/stationarySolvers.hpp"
 #include "../../lalib/src/crs/crsMatrix.hpp"
+#include "../../lalib/src/crs/crsVector.hpp"
 
 
 using namespace std;
@@ -24,39 +25,39 @@ void solver::solve(string coef_path, string rhs_path, string ret_path,
   CRSMatrix A = CRSMatrix(coef_path, 1);
 
   std::cout << "Forming the right hand side matrix ..." << "\n";
-  CRSMatrix b = CRSMatrix(rhs_path, 1);
-  CRSMatrix x0;
+  CRSVector b = CRSVector(rhs_path, 1);
+  CRSVector x0;
   
   if (init_path != "") {
     std::cout << "Forming the wanted initial guess ..." << "\n";
-    x0 = CRSMatrix(init_path);
+    x0 = CRSVector(init_path, 1);
   }
   else {
     std::cout << "Forming a 0 matrix as an initial guess ..." << "\n";
-    x0 = CRSMatrix(b.nrows(), b.ncols());
+    x0 = CRSVector(b.len());
   }
 
-  std::cout << "\n" << "Solving a " << x0.nrows() << " dimensional system ..." << "\n";
+  std::cout << "\n" << "Solving a " << x0.len() << " dimensional system ..." << "\n";
 
-  CRSMatrix ret;
+  CRSVector ret;
 
   clock_t start = clock();
   // Call the solver
   if (method == "" || method == "CG" || method == "cg") {
     std::cout << "Calling the Conjugate Gradient method ..." << "\n";
-    ret = cgSolve<CRSMatrix>(A, x0, b);
+    ret = cgSolve<CRSMatrix, CRSVector>(A, x0, b);
   }
   else if (method == "Jacobi" || method == "jacobi" || method == "JACOBI") {
     std::cout << "Calling the Jacobi method ..." << "\n";
-    ret = jacobiSolve<CRSMatrix>(A, x0, b);
+    ret = jacobiSolve<CRSMatrix, CRSVector>(A, x0, b);
   }
   else if (method == "Gauss-Seidel" || method == "gauss-seidel" || method == "GAUSS-SEIDEL" || method == "GS" || method == "gs"){
     std::cout << "Calling the Gauss-Seidel method ..." << "\n";
-    ret = gsSolve<CRSMatrix>(A, x0, b);
+    ret = gsSolve<CRSMatrix, CRSVector>(A, x0, b);
   }
   else if (method == "SOR" || method == "sor" || method == "Sor"){
     std::cout << "Calling the SOR method ..." << "\n";
-    ret = sorSolve<CRSMatrix>(A, x0, b);
+    ret = sorSolve<CRSMatrix, CRSVector>(A, x0, b);
   }
   else {
     throw runtime_error("Improper solver provided!");
@@ -67,6 +68,7 @@ void solver::solve(string coef_path, string rhs_path, string ret_path,
   
   std::cout << "Residual norm: " << (A.matmul(ret) - b).norm() << "\n\n";
 
-  std::cout << "Saving the solution as " << ret_path << " ..." << "\n";
-  ret.save(ret_path);
+  // NOTE! Save method not yet implemented
+  // std::cout << "Saving the solution as " << ret_path << " ..." << "\n";
+  // ret.save(ret_path);
 }
