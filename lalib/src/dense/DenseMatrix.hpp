d11a78226a15f0d8174fba8118c1732e7c9fec6a
krantamaki <kasper.rantamaki@gmail.com>
2023-09-10 16:05:24 +0300

    Templated dense matrix class added

---
+++ lalib/src/dense/DenseMatrix.hpp
@@ -0,0 +1,23 @@
+#ifndef DENSEMATRIX_HPP
+#define DENSEMATRIX_HPP
+
+
+/*
+  This is the file one should include when using DenseMatrix class 
+*/
+
+
+#ifndef DENSEMATRIX_DECL_HPP
+  #include "denseMatrix_decl.hpp"  // Include declaration of DenseMatrix class
+#endif
+
+#ifndef DENSEMATRIX_IMPL_HPP
+  #include "denseMatrix_impl.hpp"  // Include implementation of DenseMatrix class
+#endif
+
+#ifndef DENSEMATRIX_MATMUL_HPP
+  #include "denseMatmul.hpp"  // Include matrix multiplication for DenseMatrix class
+#endif
+
+
+#endif
