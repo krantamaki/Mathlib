575298aa088a225186f834a5dc19612189b33752
krantamaki <kasper.rantamaki@gmail.com>
2023-09-22 15:31:35 +0300

    Poisson FDM example updated to use new implementations

--- examples/poisson_fd.cpp
+++ examples/poisson_fd.cpp
@@ -1,6 +1,6 @@
 #include <chrono>
-#include "../lalib/src/crs/crsMatrix.hpp"
-#include "../lalib/src/crs/crsVector.hpp"
+#include "../lalib/src/matrix/Matrix.hpp"
+#include "../lalib/src/vector/Vector.hpp"
 #include "../lalib/src/nonstationarySolvers.hpp"
 
 
@@ -22,7 +22,7 @@
  * [1] Hannukainen, Antti. Numerical matrix computations. Course lecture notes, 2022.
  *
  * Compile at root mathlib directory with: 
- * > g++ -mavx -fopenmp -Wall lalib/src/crs/crsMatrix.cpp lalib/src/crs/crsVector.cpp lalib/src/crs/crsMatmul.cpp examples/poisson_fd.cpp -lm -o poisson_fd.o
+ * > g++ -mavx -std=c++17 -fopenmp -Wall examples/poisson_fd.cpp -lm -o poisson_fd.o
  *
  * Run with:
  * > ./poisson_fd.o
@@ -39,91 +39,117 @@ inline int ij(int i, int j, int N) {
 
 
 // Function for interior values
-double f(double x_1, double x_2) {
-  return (10.0 * x_1) * (10.0 * x_2);
+template<class type>
+type f(type x_1, type x_2) {
+  return x_1 + x_2;
 }
 
 
 // Function for boundary values
-double g(double x_1, double x_2) {
+template<class type>
+type g(type x_1, type x_2) {
   return 0.0;
 }
 
 
-int main() {
-  
-  int N = 300;  // Number of grid points in each dimension. 2D grid is then N x N
-  double h = 1.0 / ((double)N - 1.0);  // The step size
+template<class type, bool vectorize, bool sparse>
+void poissonFD(int N) {
+  type h = 1.0 / ((type)N - 1.0);  // The step size
   int N_squared = N * N;
-  double h_square = h * h;
-  double inv_h_square = 1.0 / h_square;
+  type h_square = h * h;
+  type inv_h_square = 1.0 / h_square;
 
-  std::cout << "\n" << "Solving a two dimensional Poisson problem using Finite Difference Method" << "\n";
-  std::cout << "Finite difference mesh consists of " << N << " x " << N << " points" << "\n";
-  
-  std::cout << "\n" << "Initializing the coefficient matrix and RHS vector. This may take a while..." << "\n";
+  _infoMsg("Solving a two dimensional Poisson problem using Finite Difference Method", __func__);
+
+  std::ostringstream msg1;
+  msg1 << "Finite difference mesh consists of " << N << " x " << N << " points";
+  _infoMsg(msg1.str(), __func__);
+
+  _infoMsg("Initializing the coefficient matrix and RHS vector. This may take a while...", __func__);
 
   auto init_start = std::chrono::high_resolution_clock::now();
   
   // Arrays that will define the CRS structure of the coefficient matrix
-  std::vector<double> vals;
+  std::vector<type> vals;
   std::vector<int> colInds;
   std::vector<int> rowPtrs;
   rowPtrs.push_back(0);
 
   // The RHS vector
-  CRSVector b = CRSVector(N_squared);
-  
+  Vector b = Vector<type, vectorize>(N_squared);
+
   // Form the coefficient matrix
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {      
       int row = ij(i, j, N);
-      double x_1 = (double)i / (double)N;
-      double x_2 = (double)j / (double)N;
+      type x_1 = (type)i / (type)N;
+      type x_2 = (type)j / (type)N;
 
       if ((i > 0) && (i < N - 1) && (j > 0) && (j < N - 1)) {  // Point in the interior
         vals.insert(vals.end(), {-inv_h_square, -inv_h_square, 4 * inv_h_square, -inv_h_square, -inv_h_square});
-	colInds.insert(colInds.end(), {ij(i - 1, j, N), ij(i, j - 1, N), row, ij(i, j + 1, N), ij(i + 1, j, N)});
-	rowPtrs.push_back(rowPtrs.back() + 5);
+        colInds.insert(colInds.end(), {ij(i - 1, j, N), ij(i, j - 1, N), row, ij(i, j + 1, N), ij(i + 1, j, N)});
+        rowPtrs.push_back(rowPtrs.back() + 5);
 
-	b.place(row, f(x_1, x_2));
+        b.place(row, f(x_1, x_2));
       }
       else { // Point on the boundary
         vals.push_back(1.0);
-	colInds.push_back(row);
-	rowPtrs.push_back(rowPtrs.back() + 1);
-	
-	b.place(row, g(x_1, x_2));
+        colInds.push_back(row);
+        rowPtrs.push_back(rowPtrs.back() + 1);
+
+        b.place(row, g(x_1, x_2));
       }
     }
   }
 
-  CRSMatrix A = CRSMatrix(N_squared, N_squared, vals, colInds, rowPtrs);
+  Matrix A = Matrix<type, vectorize, sparse>(N_squared, N_squared, vals, colInds, rowPtrs);
 
   auto init_end = std::chrono::high_resolution_clock::now();
-
   auto init_duration = std::chrono::duration_cast<std::chrono::milliseconds>(init_end - init_start);
 
-  std::cout << "\n" << "Time taken in preprocessing: " << init_duration.count() << " milliseconds" << "\n";
-  
-  std::cout << "\n" << "Using a zero vector as the initial guess..." << "\n";
-  CRSVector x0 = CRSVector(N_squared);
+  std::ostringstream msg2;
+  msg2 << "Time taken in preprocessing: " << init_duration.count() << " milliseconds";
+  _infoMsg(msg2.str(), __func__);
 
-  std::cout << "\n" << "Solving a " << N_squared << " dimensional system with conjugate gradient method. This may take a while..." << "\n";
+  _infoMsg("Using a zero vector as the initial guess", __func__);
+  Vector x0 = Vector<type, vectorize>(N_squared);
 
-  CRSVector ret;
+  std::ostringstream msg3;
+  msg3 << "Solving a " << N_squared << " dimensional system with conjugate gradient method. This may take a while...";
+  _infoMsg(msg3.str(), __func__);
+
+  Vector<type, vectorize> ret;
+
+  int max_iter = 10 * N;  // 10 * N chosen as an arbitrary maximum number of iterations
+  type disc_err = h * h;  // Discretization error should be O(h^2) so use it as convergence tolerance
+
+  std::ostringstream msg4;
+  msg4 << "Using discretization error " << disc_err << " as the convergence tolerance.";
+  _infoMsg(msg4.str(), __func__);
 
   auto solve_start = std::chrono::high_resolution_clock::now();
-  ret = cgSolve(A, x0, b, 10 * N);  // 10 * N chosen as an arbitrary maximum number of iterations
+  ret = cgSolve<type, vectorize, sparse>(A, x0, b, max_iter, disc_err);
   auto solve_end = std::chrono::high_resolution_clock::now();
 
   auto solve_duration = std::chrono::duration_cast<std::chrono::milliseconds>(solve_end - solve_start);
 
-  std::cout << "\n" << "Time taken by the solver: " << solve_duration.count() << " milliseconds" << "\n";
-  
-  std::cout << "Residual norm: " << (A.matmul(ret) - b).norm() << "\n\n";
+  std::ostringstream msg5;
+  msg5 << "Time taken by the solver: " << solve_duration.count() << " milliseconds";
+  _infoMsg(msg5.str(), __func__);
 
-  std::cout << "DONE" << "\n";
+  std::ostringstream msg6;
+  msg6 << "Residual norm: " << (A.matmul(ret) - b).norm();
+  _infoMsg(msg6.str(), __func__);
+}
+
+
+int main() {
+  
+  int N = 200;  // Number of grid points in each dimension. 2D grid is then N x N
+  
+  poissonFD<double, false, true>(N);
+
+  _infoMsg("DONE", __func__);
 
   return 0;
 }
